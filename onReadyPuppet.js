060c4dee4cde1ee5ad58a57b533c11a10988967b
Hitesh Tare <hiteshtare@gmail.com>
2024-03-14 16:50:07 +0530

    cleanup

--- onReadyPuppet.js
+++ onReadyPuppet.js
@@ -4,11 +4,13 @@ module.exports = async (page, scenario, vp) => {
   await require('./backstop_data/engine_scripts/puppet/clickAndHoverHelper')(page, scenario);
   console.disableLogging();
   /* 
-    Hardcoded to print Final Count of Passed/Failed  
+    Hardcoded to print Final Count of Passed/Failed:
+    
     1.scenario.sIndex value for Comparison - 
     Number of Scenarios count is 1 for to calculate index we take Total count - 1 
     i.e. 1-1 = 0
     Hence, (scenario.sIndex == 0)
+    
     2.vp.vIndex value for Comparison - 
     Number of View Port count is 2 for to calculate index we take Total count - 1 
     i.e. 2-1 = 1
